da39c942a59eaad45f581a6a6559eab5a5491c39
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-05-16 00:12:32 +0300

    updating footer styles

--- src/layouts/mainLayout/footer.js
+++ src/layouts/mainLayout/footer.js
@@ -7,8 +7,13 @@ const Footer = () => {
     <div bg="dark" className="w-100 bg-dark position-absolute">
       <p className="m-0 text-center text-white py-3">
         By @
-        <span className="text-danger">
-          <a href="https://github.com/Mazin-emad">Mazin Emad</a>
+        <span>
+          <a
+            className="text-danger text-decoration-none"
+            href="https://github.com/Mazin-emad"
+          >
+            Mazin Emad
+          </a>
         </span>{" "}
         ©{year}
       </p>
